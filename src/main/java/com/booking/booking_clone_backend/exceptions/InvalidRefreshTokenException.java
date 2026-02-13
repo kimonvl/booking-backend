5719f99d17fcc89918a4ecd05155c88373e9b1ef
kimonvl <kimonvloutis@gmail.com>
2026-02-13 19:04:26 +0200

    chore: Remove unused exceptions

--- src/main/java/com/booking/booking_clone_backend/exceptions/InvalidRefreshTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.booking.booking_clone_backend.exceptions;
-
-public class InvalidRefreshTokenException extends RuntimeException {
-    public InvalidRefreshTokenException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/booking/booking_clone_backend/exceptions/MissingRefreshTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.booking.booking_clone_backend.exceptions;
-
-public class MissingRefreshTokenException extends RuntimeException {
-    public MissingRefreshTokenException(String message) {
-        super(message);
-    }
-}
